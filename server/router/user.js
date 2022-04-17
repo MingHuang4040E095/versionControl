959f0beff4e61160b80ed9c7f9c3abe8ffa8af77
MingHuang1220 <syuminghuang@gmail.com>
2022-04-17 17:12:53 +0800

    補上新增使用者日期&新增後建立使用者的資料夾

--- server/router/user.js
+++ server/router/user.js
@@ -1,23 +1,37 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require("bcryptjs");
+const fs = require('fs') // 檔案相關套件
+
 //-------------------- 資料庫 ----------------------
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const UserModelSchema = new Schema({
     account:{type:String,required:true}, // 使用者帳號
     password:{type:String,required:true}, // 密碼
-    dateCreated: { type: Date, required: true }, //建立時間
+    dateCreated: { type: Date, required: true,default:Date.now }, //建立時間
 })
 // 這邊的user是資料表名稱
 const UserModel = mongoose.model('user', UserModelSchema)
 //----------------------------------------------------
+/**
+ * 建立資料夾
+ * @param {[String]} folderName 資料夾名稱
+ */
+function folderCreate(folderName){
+    fs.mkdir(`${__dirname}/../file/${folderName}`,function(err){
+        if(err) console.log(err)
+        return err ? false : true
+    })
+}
 
 // 新增使用者
 router.post('/add',function(req,res){
+    console.log(req.body)
     //雜湊
     bcrypt.hash(req.body.password, 11, (err, hash) => {
         if(err){ 
+            console.log(err)
             res.json({
                 status: false
             })
@@ -26,7 +40,10 @@ router.post('/add',function(req,res){
                 account: req.body.account,
                 password: hash,
             }).save((err, result) => {
-                status = err ? false : true
+                console.log(result)
+                let status = err ? false : true
+
+                if(status) folderCreate(result._id) // 建立資料夾
                 res.json({
                     status: status,
                     data:result
@@ -38,14 +55,10 @@ router.post('/add',function(req,res){
 
 // 查詢使用者
 router.get('/list',function(req,res){
-    UserModel.aggregate([
-        { 
-            $group: { 
-                _id:'$_id',
-                account: { $last: "$account" },
-            } 
-        },
-    ]).exec(function(err,result){
+    console.log(`${__dirname}/../file`)
+    UserModel.find({},{
+        password:0 // 不要顯示password這個欄位
+    }).exec(function(err,result){
         res.json({
             status: err ? false : true,
             data: err ? [] : result,
