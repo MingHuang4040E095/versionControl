c5a0f7ca7ae2a33d9e6910624adf50ce828ad63e
MingHuang1220 <syuminghuang@gmail.com>
2022-04-17 16:20:43 +0800

    加入查詢所有使用者&使用者詳細資料api

--- server/router/user.js
+++ server/router/user.js
@@ -1,25 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const bcrypt = require("bcryptjs");
-
-
-// // create application/json parser
-// const jsonParser = bodyParser.json()
-// // create application/x-www-form-urlencoded parser
-// const urlencodedParser = bodyParser.urlencoded({ extended: false })
 //-------------------- 資料庫 ----------------------
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const UserModelSchema = new Schema({
     account:{type:String,required:true}, // 使用者帳號
     password:{type:String,required:true}, // 密碼
+    dateCreated: { type: Date, required: true }, //建立時間
 })
-//----------------------------------------------------
-// 這邊的files是資料表名稱
+// 這邊的user是資料表名稱
 const UserModel = mongoose.model('user', UserModelSchema)
+//----------------------------------------------------
 
+// 新增使用者
 router.post('/add',function(req,res){
-    
     //雜湊
     bcrypt.hash(req.body.password, 11, (err, hash) => {
         if(err){ 
@@ -40,6 +35,49 @@ router.post('/add',function(req,res){
         }
     });
 })
+
+// 查詢使用者
+router.get('/list',function(req,res){
+    UserModel.aggregate([
+        { 
+            $group: { 
+                _id:'$_id',
+                account: { $last: "$account" },
+            } 
+        },
+    ]).exec(function(err,result){
+        res.json({
+            status: err ? false : true,
+            data: err ? [] : result,
+        })
+    })
+    // UserModel.find().exec(function(err,result){
+    //     res.json({
+    //         status:err?false:true,
+    //         data: err? [] : result
+    //     })
+    // })
+})
+
+// 查詢使用者詳細資料
+router.get('/detail/userID=:userID',function(req,res){
+    UserModel.aggregate([
+        { 
+            $group: { 
+                _id:'$_id',
+                account: { $last: "$account" },
+            } 
+        },
+    ]).exec(function(err,result){
+        console.log(result)
+        res.json({
+            status:err?false:true,
+            data: result
+        })
+    })
+})
+
+
 
 
 
